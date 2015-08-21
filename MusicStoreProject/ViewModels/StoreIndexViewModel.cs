0c8318adc584976dd7eab7930c9d6c81d659f21a
Potego Malatji <potego@bbd.co.za>
2015-08-21 13:44:22 +0200

    test

---
+++ MusicStoreProject/ViewModels/StoreIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreProject.ViewModels
+{
+    public class StoreIndexViewModel
+    {
+        public int NumberOfGenres { get; set; }
+
+        public List<string> Genres { get; set; }
+    }
+}
